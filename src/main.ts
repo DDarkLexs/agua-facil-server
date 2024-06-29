7eeec00aaf3880aa2169f6f593c0cfbaf2a430ca
DDark <antoniolexis2001@outlook.com>
2024-06-30 00:59:33 +0100

    init ✨🛠️

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+/* 
+
+ ███▄    █ ▓█████   ██████ ▄▄▄█████▓    ▄▄▄██▀▀▀██████ 
+ ██ ▀█   █ ▓█   ▀ ▒██    ▒ ▓  ██▒ ▓▒      ▒██ ▒██    ▒ 
+▓██  ▀█ ██▒▒███   ░ ▓██▄   ▒ ▓██░ ▒░      ░██ ░ ▓██▄   
+▓██▒  ▐▌██▒▒▓█  ▄   ▒   ██▒░ ▓██▓ ░    ▓██▄██▓  ▒   ██▒
+▒██░   ▓██░░▒████▒▒██████▒▒  ▒██▒ ░     ▓███▒ ▒██████▒▒
+░ ▒░   ▒ ▒ ░░ ▒░ ░▒ ▒▓▒ ▒ ░  ▒ ░░       ▒▓▒▒░ ▒ ▒▓▒ ▒ ░
+░ ░░   ░ ▒░ ░ ░  ░░ ░▒  ░ ░    ░        ▒ ░▒░ ░ ░▒  ░ ░
+   ░   ░ ░    ░   ░  ░  ░    ░          ░ ░ ░ ░  ░  ░  
+         ░    ░  ░      ░               ░   ░       ░  
+                                                       
+*/
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new IoAdapter(app));
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT || 1005);
+}
+bootstrap();
