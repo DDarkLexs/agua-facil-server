936cf3849a61e1d7ed8b06a671b0cc16d36f2ea4
DDark <antoniolexis2001@outlook.com>
2024-07-05 23:07:20 +0100

    em desenvolvimento 🛠️✨

--- src/servico/dto/update-servico.dto.ts
+++ src/servico/dto/update-servico.dto.ts
@@ -1,4 +1,3 @@
-import { PartialType } from '@nestjs/mapped-types';
 import { CreateServicoDto } from './create-servico.dto';
 
-export class UpdateServicoDto extends PartialType(CreateServicoDto) {}
+export class UpdateServicoDto extends CreateServicoDto {}
