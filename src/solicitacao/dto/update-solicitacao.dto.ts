ad325d4a2cad341819e5479aee928df6cc1b75f0
DDark <antoniolexis2001@outlook.com>
2024-07-19 17:05:30 +0100

    continuação de desenvolvimento do serviço e implementação do Socket.io 🛠✨

--- src/solicitacao/dto/update-solicitacao.dto.ts
+++ src/solicitacao/dto/update-solicitacao.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateSolicitacaoDto } from './create-solicitacao.dto';
+import { IsEnum } from 'class-validator';
+import { $Enums } from '@prisma/client';
 
-export class UpdateSolicitacaoDto extends PartialType(CreateSolicitacaoDto) {}
+export class UpdateSolicitacaoDto extends PartialType(CreateSolicitacaoDto) {
+  @IsEnum($Enums.ServicoStatus, { message: 'O valor fornecido para o campo "status" é inválido. Por favor, forneça um dos valores válidos: "PENDENTE", "ACEITO", "CONCLUIDO" ou "RECUSADO".' })
+  status:  $Enums.ServicoStatus
+    
+}
+
+
+export class UpdateStatusSolicitacaoDto {
+  @IsEnum($Enums.ServicoStatus, { message: 'O valor fornecido para o campo "status" é inválido. Por favor, forneça um dos valores válidos: "PENDENTE", "ACEITO", "CONCLUIDO" ou "RECUSADO".' })
+  status:  $Enums.ServicoStatus
+}
